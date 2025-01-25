29b963234107a0d39b40713157aea22ed33e85c7
hyobin <hbaloha@naver.com>
2025-01-25 17:34:10 +0900

    Validation logic check

--- components/auth/SignUpForm.tsx
+++ components/auth/SignUpForm.tsx
@@ -1,9 +1,25 @@
+'use client';
 import { Label } from '@radix-ui/react-label';
 import { FormCard } from './FormCard';
 import { Input } from '../ui/input';
 import { Submit } from './Submit';
+import { SignUpSchema } from '@/schemas/auth';
+import { useFormValidate } from '@/hooks/useFormValidate';
+import { TSignUpFormError } from '@/types/form';
+import { FormMessage } from './FormMessage';
+import { ChangeEvent } from 'react';
 
 export function SignUpForm() {
+  const { errors, validateField } =
+    useFormValidate<TSignUpFormError>(SignUpSchema);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    validateField(name, value);
+  };
+
+  console.log('errors', errors);
+
   return (
     <FormCard
       title="회원가입"
@@ -12,7 +28,14 @@ export function SignUpForm() {
       <form className="space-y-6">
         <div className="space-y-1">
           <Label htmlFor="name">이름</Label>
-          <Input id="name" name="name" placeholder="이름을 입력해주세요" />
+          <Input
+            id="name"
+            name="name"
+            placeholder="이름을 입력해주세요"
+            error={!!errors?.name}
+            onChange={handleChange}
+          />
+          {errors?.name && <FormMessage message={errors?.name[0]} />}
         </div>
         <div className="space-y-1">
           <Label htmlFor="email">이메일</Label>
@@ -21,7 +44,10 @@ export function SignUpForm() {
             name="email"
             type="email"
             placeholder="example@example.com"
+            error={!!errors?.email}
+            onChange={handleChange}
           />
+          {errors?.email && <FormMessage message={errors?.email[0]} />}
         </div>
         <div className="space-y-1">
           <Label htmlFor="password">비밀번호</Label>
@@ -30,7 +56,10 @@ export function SignUpForm() {
             name="password"
             type="password"
             placeholder="********"
+            error={!!errors?.password}
+            onChange={handleChange}
           />
+          {errors?.password && <FormMessage message={errors?.password[0]} />}
         </div>
         <Submit className="w-full">가입하기</Submit>
       </form>
