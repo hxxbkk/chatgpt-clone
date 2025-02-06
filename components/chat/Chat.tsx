e15afa741a8267ed8921962dcad4d59437e4fa99
hyobin <hbaloha@naver.com>
2025-02-06 16:22:30 +0900

    input markup

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -1,5 +1,10 @@
+'use client';
+
+import { useState } from 'react';
+import { AutoResizingTextarea } from './AutoResizingTextarea';
 import { Empty } from './Empty';
 import { Message } from './Message';
+import { ArrowUp } from 'lucide-react';
 
 const MESSAGE_DUMMY = [
   { id: '1', content: '더미데이터1', role: 'user' },
@@ -7,6 +12,7 @@ const MESSAGE_DUMMY = [
 ];
 
 export function Chat() {
+  const [value, setValue] = useState('');
   return (
     <div className="flex flex-col w-[80%] h-full mx-auto">
       {/* 채팅영역 */}
@@ -28,6 +34,17 @@ export function Chat() {
       </div>
 
       {/* input영역 */}
+      <div className="pb-5">
+        <form className="flex itmes-center justify-center gap-4">
+          <AutoResizingTextarea
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button type="submit" size="icon">
+            <ArrowUp />
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
