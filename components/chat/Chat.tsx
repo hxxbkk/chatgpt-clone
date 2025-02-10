6de0d5702dfa564185725474b3590c5c520d4a1e
hyobin <hbaloha@naver.com>
2025-02-10 16:58:02 +0900

    open ai api

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
+import { useChat } from 'ai/react';
 import { AutoResizingTextarea } from './AutoResizingTextarea';
 import { Empty } from './Empty';
 import { Message } from './Message';
 import { Button } from '../ui/button';
 import { ArrowUp } from 'lucide-react';
 import { DUMMY_LONG_TEXT } from '@/constants/dummy';
+import { useModelStore } from '@/store/model';
 
 const MESSAGE_DUMMY = [
   { id: '1', content: '더미데이터1', role: 'user' },
@@ -16,24 +18,25 @@ const MESSAGE_DUMMY = [
 ];
 
 export function Chat() {
-  const [value, setValue] = useState('');
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const model = useModelStore((state) => state.model);
   const scrollRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (scrollRef.current) {
       scrollRef.current.scrollIntoView({ behavior: 'smooth' });
     }
-  }, []);
+  }, [messages]);
 
   return (
     <div className="flex flex-col w-[80%] h-full mx-auto">
       {/* 채팅영역 */}
       <div className="flex-1">
-        {MESSAGE_DUMMY.length === 0 ? (
+        {messages.length === 0 ? (
           <Empty />
         ) : (
           <>
-            {MESSAGE_DUMMY.map((message) => (
+            {messages.map((message) => (
               <Message
                 key={message.id}
                 name={'user'}
@@ -47,11 +50,11 @@ export function Chat() {
 
       {/* input영역 */}
       <div className="pb-5 sticky bottom-0 bg-white">
-        <form className="flex items-center justify-center gap-4">
-          <AutoResizingTextarea
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
+        <form
+          className="flex items-center justify-center gap-4"
+          onSubmit={(e) => handleSubmit(e, { data: model })}
+        >
+          <AutoResizingTextarea value={input} onChange={handleInputChange} />
           <Button type="submit" size="icon">
             <ArrowUp />
           </Button>
