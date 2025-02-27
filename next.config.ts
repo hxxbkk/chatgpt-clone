b17414b4d6b313c7fc18cc18fad3e9b780e36878
hyobin <hbaloha@naver.com>
2025-02-27 15:19:43 +0900

    Trigger Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    // '@' 별칭을 프로젝트의 루트 디렉토리로 설정
+    config.resolve.alias['@'] = path.resolve(__dirname);
+
+    return config;
+  },
 };
 
 export default nextConfig;
